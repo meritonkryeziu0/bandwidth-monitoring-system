3c78075ee40ae88e564eb6a69fe3472b7872930a
Melisa Alaj <94481939+melisaalaj@users.noreply.github.com>
2023-01-07 16:18:57 +0100

    Updating the GUI Appearance

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from datetime import datetime
 from tkinter import *
+from PIL import ImageTk, Image
 
+
+def export_action():
+    # TODO implement export_action() 
+    pass
+
+
+def visualize_action():
+    # TODO implement visualize_action()
+    pass
+
+def close_action():
+    # TODO implement close_action()
+    pass
 
 class APP:
     # globals
@@ -14,17 +28,33 @@ class APP:
         self.startTime = datetime.now()
         self.window = tk
 
-        self.labels = {"download-header": Label(text="Download Speed", font="Monospace 12 bold"),
-                       "download": Label(text="Measuring...", font="Monospace 12 bold"),
-                       "upload-header": Label(text="Upload Speed", font="Monospace 12 bold"),
-                       "upload": Label(text="Measuring...", font="Monospace 11 bold")
-                       }
+        global logo
+        image = Image.open("photos/logo.jpg")
+        img = image.resize((122, 78))
+        logo = ImageTk.PhotoImage(img)
+        self.photo = Label(image=logo)
 
+        self.labels = {"download-header": Label(text="Download Speed", font="Arial 13 bold"),
+                       "download": Label(text="Measuring...", font="Arial 11 bold", fg="#0e4373", pady=5),
+                       "upload-header": Label(text="Upload Speed", font="Arial 13 bold"),
+                       "upload": Label(text="Measuring...", font="Arial 11 bold", fg="#0e4373", pady=5), }
+
+        self.exportBtn = Button(tk, text="Export", font="Arial 9 bold", padx=20, background="#145DA0",
+                                foreground="white", activebackground="#0e4373", command=export_action)
+        self.visualizeBtn = Button(tk, text="Visualize", font="Arial 9 bold", padx=12, background="#145DA0",
+                                   foreground="white", activebackground="#0e4373", command=visualize_action)
+        self.closeBtn = Button(tk, text="Close", font="Arial 9 bold", padx=21, background="#145DA0", foreground="white",
+                               activebackground="#0e4373", command=close_action)
+    
     def setup(self):
+        self.photo.pack()
         self.labels["download-header"].pack()
         self.labels["download"].pack()
         self.labels["upload-header"].pack()
         self.labels["upload"].pack()
+        self.exportBtn.pack()
+        self.visualizeBtn.pack()
+        self.closeBtn.pack()
         self.tk.update()
 
     def set_window_properties(self):
