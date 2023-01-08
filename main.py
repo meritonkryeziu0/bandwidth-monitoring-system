ee97d83fda7079fd8fca937cf77350eec6dff07c
meritonkryeziu0 <meritonkryeziu0@gmail.com>
2023-01-08 16:51:46 +0100

    Improvement and cleanup

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from tkinter import *
 from app import app as source_app 
 
 if __name__ == '__main__':
-    print("Its working!")
     window = Tk()
     app = source_app.APP(window)
+
     app.set_window_properties()
     app.update_window()
     app.setup()
--- visualize/VisualizeLoggedData.py
+++ visualize/VisualizeLoggedData.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def plot_graph():
     colnames = ['Datetime', 'Download', 'Upload']
     df = pd.read_csv('log.csv', names=colnames)
     df['Download'].plot()
     df['Upload'].plot()
     plt.legend(['Download', 'Upload'])
+    plt.ylabel('Bandwidth (MiB)')
+    plt.xlabel('Time (s)')
     plt.show()
